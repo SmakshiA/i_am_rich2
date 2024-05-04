5c99b398add15468712f2dd0d75196a6e10b37f4
smakshi <smakshina1306@gmail.com>
2024-05-04 23:06:34 +0530

    Done 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
         title: Text(
@@ -14,7 +15,7 @@ void main() {
       backgroundColor: Colors.white70,
       body: Center(
         child: Image(
-          image: NetworkImage('https://img.freepik.com/premium-photo/abstract-design-ambitious-girl-with-buildings-background_949004-238.jpg?w=740'),
+          image: AssetImage('images/diamond.png'),
         ),
       )
     )
