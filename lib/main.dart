0feb2145d137dd492c040e2e81db9274ccddea22
smakshi <smakshina1306@gmail.com>
2024-05-04 19:06:47 +0530

    Main commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,18 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         title: Text(
-          "I am Rich Again"
+          'I am Rich Again'
         ),
         backgroundColor: Colors.blueGrey.shade900,
         centerTitle: true,
         foregroundColor: Colors.white,
       ),
       backgroundColor: Colors.white70,
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://img.freepik.com/premium-photo/abstract-design-ambitious-girl-with-buildings-background_949004-238.jpg?w=740'),
+        ),
+      )
     )
   ));
 }
